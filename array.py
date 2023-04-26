93ac05cf1987f2e89f96fc40100af7a544c2c4e0
Alina Babenko <117819807+cyberprince2201@users.noreply.github.com>
2023-04-26 09:26:47 +0300

    New code

--- array.py
+++ array.py
@@ -1,2 +1,29 @@
+import ctypes
+class Array:
+
+    def __init__(self, size) -> None:
+        self.size = size
+        self._n = 0
+        self._array = (ctypes.py_object * self.size)()
+    
+    def __length__(self):
+        return self._n
+    
+    def add_item(self, item):
+        if self._n < self.size:
+            self._array[self._n] = item
+            self._n = self._n + 1
+        else:
+            return "Out of range"
+
+    def get_item(self, idex):
+        if idex < self._n:
+            return self._array[idex]
+        else:
+            return "Out of range"
+    
+    def set_item(self, idex, value):
+
+
 def clear(self, value):
     
